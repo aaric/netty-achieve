13703bb13b207a71029264082732f2dd65f62dfb
aaric <vipaaric@gmail.com>
2018-04-28 17:13:16 +0800

    0.0.1-SNAPSHOT

---
+++ src/test/java/NettyTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * NettyTest
+ *
+ * @author Aaric, created on 2018-04-28T16:39.
+ * @since 0.0.1-SNAPSHOT
+ */
+public class NettyTest {
+
+    @Test
+    public void testPrint() {
+        System.out.println("hello");
+    }
+}
