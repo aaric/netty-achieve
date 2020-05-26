a96a9a609bfe01a122eeff1cddef6c7a7b88a035
qlhan <qlhan@incarcloud.com>
2020-05-26 10:32:53 +0800

    1.4.0-SNAPSHOT webflux sse

--- netty-gateway/src/main/java/com/incarcloud/boar/controller/FluxController.java
+++ netty-gateway/src/main/java/com/incarcloud/boar/controller/FluxController.java
@@ -1,9 +1,18 @@
 package com.incarcloud.boar.controller;
 
+import com.incarcloud.boar.service.FluxService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
+
+import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * WebFlux Test
@@ -11,15 +20,46 @@ import reactor.core.publisher.Mono;
  * @author Aaric, created on 2020-05-26T09:29.
  * @version 1.4.0-SNAPSHOT
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/flux")
 public class FluxController {
 
-    /**
-     * 同步非阻塞
-     */
-    @GetMapping("/mono")
-    public Mono<String> mono() {
+    @Autowired
+    private FluxService fluxService;
+
+    @GetMapping("/just1")
+    public Mono<String> just1() {
         return Mono.just("Hello WebFlux");
+    }
+
+    @GetMapping("/sub")
+    public Mono<Object> sub() {
+        return Mono.create(sink -> {
+            log.debug("sink: {}", sink);
+            sink.success("Hello WebFlux");
+        }).doOnSubscribe(sub -> {
+            log.info("sub: {}", sub);
+        }).doOnNext(o -> {
+            log.debug("o: {}", o);
+        });
+    }
+
+    @GetMapping("/just2")
+    public Flux<String> just2() {
+        return Flux.just("Hello", "Web", "Flux");
+    }
+
+    @GetMapping("/sse")
+    public Flux<ServerSentEvent<Integer>> sse() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(seq -> Tuples.of(seq, ThreadLocalRandom.current().nextInt()))
+                .map(data -> {
+                    ServerSentEvent.Builder<Integer> builder = ServerSentEvent.builder();
+                    return builder.event("random")
+                            .id(Long.toString(data.getT1()))
+                            .data(data.getT2())
+                            .build();
+                });
     }
 }
