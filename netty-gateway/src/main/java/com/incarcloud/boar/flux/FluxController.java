c3ac0df32b6f9ec57e12e917c8bc5564418f88f0
qlhan <qlhan@incarcloud.com>
2020-05-27 12:51:47 +0800

    1.4.0-SNAPSHOT webflux sse

--- netty-gateway/src/main/java/com/incarcloud/boar/flux/FluxController.java
+++ netty-gateway/src/main/java/com/incarcloud/boar/flux/FluxController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuples;
 
 import java.time.Duration;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.UUID;
 
 /**
  * FluxController
@@ -59,19 +58,25 @@ public class FluxController {
     @GetMapping(value = "/sse1", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> sse1() {
         return Flux.interval(Duration.ofSeconds(1))
-                .map(val -> "-> " + val);
+                .map(data -> "-> " + data);
     }
 
     @GetMapping("/sse2")
-    public Flux<ServerSentEvent<Integer>> sse2() {
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(seq -> Tuples.of(seq, ThreadLocalRandom.current().nextInt()))
-                .map(data -> {
-                    ServerSentEvent.Builder<Integer> builder = ServerSentEvent.builder();
-                    return builder.event("random")
-                            .id(Long.toString(data.getT1()))
-                            .data(data.getT2())
-                            .build();
-                });
+    public Flux<ServerSentEvent<String>> sse2() {
+        /*Flux<Long> flux = Flux.interval(Duration.ofSeconds(1));
+        Flux<ServerSentEvent<String>> event = flux.map(data -> {
+            return ServerSentEvent.<String>builder()
+                    .id(UUID.randomUUID().toString())
+                    .event("random")
+                    .data(data.toString())
+                    .build();
+        });*/
+        return Flux.interval(Duration.ofSeconds(1)).map(data ->
+                ServerSentEvent.<String>builder()
+                        .id(UUID.randomUUID().toString())
+                        .event("random")
+                        .data(data.toString())
+                        .build()
+        );
     }
 }
